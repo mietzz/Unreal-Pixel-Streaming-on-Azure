5a71403ddac86d2b5e73cf3b1e50aeb0f64522a1
Dan Manrique <danmanrique@hotmail.com>
2020-10-26 17:03:52 -0400

    working a bug on the mm extension

--- scripts/setupMatchMakerVM.ps1
+++ scripts/setupMatchMakerVM.ps1
@@ -10,14 +10,20 @@ choco install directx -yr --no-progress
 
 New-Alias -Name git -Value "$Env:ProgramFiles\Git\bin\git.exe" -Force
 
+export GITHUB_USER=anonuser
+export GITHUB_TOKEN=(az keyvault secret show -n thekey --vault-name uegamingakv | ConvertFrom-Json).value
+export GITHUB_REPOSITORY=Azure/Unreal-Pixel-Streaming-on-Azure
+
 $folder = "c:\Unreal\"
 if (-not (Test-Path -LiteralPath $folder)) {
-    git clone -q https://github.com/Azure/Unreal-Pixel-Streaming-on-Azure.git $folder
+    #git clone -q https://github.com/Azure/Unreal-Pixel-Streaming-on-Azure.git $folder
+    git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY} $folder
 }
 else {
     #rename the existing folder
     $endtag = 'unreal-' + (get-date).ToString('MMddyyhhmmss')
     Rename-Item -Path $folder  -NewName $endtag -Force
-    git clone -q https://github.com/Azure/Unreal-Pixel-Streaming-on-Azure.git $folder
+    #git clone -q https://github.com/Azure/Unreal-Pixel-Streaming-on-Azure.git $folder
+    git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY} $folder
 }
 exit 0
