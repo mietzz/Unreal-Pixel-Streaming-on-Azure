85205f30e722e444c7c0893bd47425296e5f1d11
Dan Manrique <danmanrique@hotmail.com>
2020-11-19 21:47:39 -0500

    added new scripts from main repo

---
+++ scripts/DeleteAll.ps1
@@ -0,0 +1,30 @@
+#this script is to delete an environment
+
+#variables
+$rootvariable = "jk07k"
+$vnetname = "jumpbox-vnet" 
+#$vnetname = "jumpbox-fork-vnet" 
+
+#script
+
+#step 1 delete the resource groups
+$r1 = $rootvariable + "-eastus-unreal-rg"
+az group delete --name $r1 --no-wait --yes
+
+$r2 = $rootvariable + "-westeurope-unreal-rg"
+az group delete --name $r2 --no-wait --yes
+
+$r3 = $rootvariable + "-westus-unreal-rg"
+az group delete --name $r3 --no-wait --yes
+
+$r4 = $rootvariable + "-southeastasia-unreal-rg"
+az group delete --name $r4 --no-wait --yes
+
+$r5 = $rootvariable + "-global-unreal-rg"
+az group delete --name $r5 --no-wait --yes
+
+#step 2 delete the resource peerings if they exist
+az network vnet peering delete --name LinkVnet1ToVnet2 --resource-group OtherAssets --vnet-name $vnetname
+az network vnet peering delete --name LinkVnet1ToVnet3 --resource-group OtherAssets --vnet-name $vnetname
+az network vnet peering delete --name LinkVnet1ToVnet4 --resource-group OtherAssets --vnet-name $vnetname
+az network vnet peering delete --name LinkVnet1ToVnet5 --resource-group OtherAssets --vnet-name $vnetname
