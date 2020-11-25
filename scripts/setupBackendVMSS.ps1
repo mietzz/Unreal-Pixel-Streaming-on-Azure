0e89eff4755f93c880b882de0711d3625fb0516e
Neerja Ginotra <neginotr@microsoft.com>
2020-11-25 09:50:05 -0800

    Adding a sleep as sometimes process is not running

--- scripts/setupBackendVMSS.ps1
+++ scripts/setupBackendVMSS.ps1
@@ -159,6 +159,9 @@ $logmessage = "Downloading WindowsNoEditor binaries from blob storage"
 Add-Content -Path $logoutput -Value $logmessage
 
 Invoke-WebRequest $zipfilepath -OutFile $zipfilename 
+# Adding a wait for zipfile download to complete
+#sleep for 15 seconds to wait for install processes to complete
+Start-Sleep -s 15
 
 $logmessage = "Downloading WindowsNoEditor binaries from blob storage complete"
 Add-Content -Path $logoutput -Value $logmessage
@@ -166,6 +169,10 @@ Add-Content -Path $logoutput -Value $logmessage
 $logmessage = "Extracting WindowsNoEditor to " + $blobDestination
 Add-Content -Path $logoutput -Value $logmessage
 Expand-Archive -LiteralPath $zipFileName -DestinationPath $blobDestination -force
+
+# Adding a wait for zipfile download to complete
+#sleep for 15 seconds to wait for install processes to complete
+Start-Sleep -s 15
 
 $logmessage = "Extracting WindowsNoEditor Complete"
 Add-Content -Path $logoutput -Value $logmessage
@@ -177,6 +184,10 @@ Add-Content -Path $logoutput -Value $logmessage
 try{
   Copy-Item $projectExecFolder $blobDestination -recurse -force
   $logmessage = "Copying WindowsNoEditor Folder Complete"
+
+  # Adding a wait for zipfile download to complete
+  #sleep for 15 seconds to wait for install processes to complete
+  Start-Sleep -s 15
   Write-Output $logmessage
   Add-Content -Path $logoutput -Value $logmessage
 }
