2d7bbc63685217f0484dc104e6ab279c1fdc8d44
Ben Humphrey <behumphr@microsoft.com>
2020-11-27 16:34:16 -0600

    Put choco installs on each line

--- scripts/setupMatchMakerVM.ps1
+++ scripts/setupMatchMakerVM.ps1
@@ -74,7 +74,11 @@ finally {
 try {
   $logmessage = "Installing prerequisites"
   Add-Content -Path $logoutput -Value $logmessage
-  choco upgrade filezilla git nodejs vcredist2017 directx -y --no-progress
+  choco upgrade filezilla -y --no-progress
+  choco upgrade git -y --no-progress
+  choco upgrade nodejs -y --no-progress
+  choco upgrade vcredist2017 -y --no-progress
+  choco upgrade directx -y --no-progress
   $logmessage = "Installing prerequisites Complete"
   Add-Content -Path $logoutput -Value $logmessage
 }
