7a15bb9034936dac823d33c11339fd3ba8f76b79
Dan Manrique <danmanrique@hotmail.com>
2020-11-30 16:23:32 -0500

    updated BringDownCompute script

--- scripts/BringDownCompute.ps1
+++ scripts/BringDownCompute.ps1
@@ -1,8 +1,14 @@
 Param (
     [Parameter(Mandatory = $True, HelpMessage = "5 Character Env Prefix")]
     [String]$rootvariable = "",
-    [Parameter(Mandatory = $True, HelpMessage = "Instances")]
-    [int]$instances = 0
+    [Parameter(Mandatory = $True, HelpMessage = "East US Instances")]
+    [int]$eastusinstances = 0,
+    [Parameter(Mandatory = $True, HelpMessage = "West US Instances")]
+    [int]$westusinstances = 0,
+    [Parameter(Mandatory = $True, HelpMessage = "West Europe Instances")]
+    [int]$weinstances = 0,
+    [Parameter(Mandatory = $True, HelpMessage = "SouthEast Asia Instances")]
+    [int]$seainstances = 0
 )
 
 #check for 5 characters
@@ -12,8 +18,23 @@ if ($rootvariable.Length -ne 5) {
 }
 
 #check for an int between 0 and 200
-If ($instances -lt 0 -OR $instances -gt 200) {
+If ($eastusinstances -lt 0 -OR $eastusinstances -gt 200) {
     Write-Output "The second value is not a between 0 and 200"
+    return
+}
+
+If ($westusinstances -lt 0 -OR $westusinstances -gt 200) {
+    Write-Output "The third value is not a between 0 and 200"
+    return
+}
+
+If ($weinstances -lt 0 -OR $weinstances -gt 200) {
+    Write-Output "The fourth value is not a between 0 and 200"
+    return
+}
+
+If ($seainstances -lt 0 -OR $seainstances -gt 200) {
+    Write-Output "The fifth value is not a between 0 and 200"
     return
 }
 
@@ -25,7 +46,10 @@ $rg4 = $rootvariable + "-southeastasia-unreal-rg"
 $vm = $rootvariable + "-mm-vm0"
 $vmss = $rootvariable + "vmss"
 
-function handleenv($rg) {
+function handleenv() {
+
+    $rg = $args[0]
+    $instances = $args[1]
 
     Write-Output ""
     Write-Output "----------------------------------"
@@ -36,22 +60,22 @@ function handleenv($rg) {
 
     #change instance count to 2
     Write-Output "Scaling VMSS to: " + $instances.ToString()
-    #    az vmss scale --name $vmss --new-capacity $instances --resource-group $rg --only-show-errors
+    az vmss scale --name $vmss --new-capacity $instances --resource-group $rg --only-show-errors
 
     #restart vm
     Write-Output "Restarting VM"
-    #    az vm restart -g $rg -n $vm --only-show-errors
+    az vm restart -g $rg -n $vm --only-show-errors
 
     #restart vmss
     Write-Output "Restarting VMSS"
-    #    az vmss restart --name $vmss --resource-group $rg --only-show-errors
+    az vmss restart --name $vmss --resource-group $rg --only-show-errors
     return
 }
 
-handleenv($rg1)
-handleenv($rg2)
-handleenv($rg3)
-handleenv($rg4)
+handleenv $rg1 $eastusinstances;
+handleenv $rg2 $weinstances;
+handleenv $rg3 $westusinstances;
+handleenv $rg4 $seainstances;
 
 Write-Output "Date/Time (UTC): " + (get-date).ToString('MM/dd/yy hh:mm:ss')
 Write-Output "Processing Complete"
