e5eef29a512f79634d35c19e5a83eb0e36cd7ab5
mietzz <info@david-joerg.com>
2021-02-08 09:19:52 +0100

    changed vmss premium -> standard to minimize costs

--- iac/compute/vmss/main.tf
+++ iac/compute/vmss/main.tf
@@ -116,7 +116,7 @@ resource "azurerm_windows_virtual_machine_scale_set" "vmss" {
   }
 
   os_disk {
-    storage_account_type = "Premium_LRS"
+    storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
   }
 
